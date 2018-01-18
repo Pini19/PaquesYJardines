cb555a0fe0859019fb8b463f1f5e98bd3669e89f
Daniel Del Rio Pinilla <daniel_ndo@hotmail.com>
2018-01-19 00:00:07 +0100

    Añadido el intebt a google maps

--- app/src/main/java/com/example/android/parquesyjardines/InfoActivity.java
+++ app/src/main/java/com/example/android/parquesyjardines/InfoActivity.java
@@ -2,10 +2,13 @@ package com.example.android.parquesyjardines;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,7 +43,7 @@ public class InfoActivity extends AppCompatActivity {
         description = this.findViewById(R.id.textoDesc);
         address = this.findViewById(R.id.textoDir);
         schedule = this.findViewById(R.id.textoHorario);
-        loc = this.findViewById(R.id.enlaceLoc);
+        //loc = this.findViewById(R.id.enlaceLoc);
         name = this.findViewById(R.id.textoNombre);
         url = this.findViewById(R.id.enlaceUrl);
 
@@ -61,11 +64,27 @@ public class InfoActivity extends AppCompatActivity {
 
         address.setText(myMap.get(ClavesPyJ.direccion.toString()).toString());
         schedule.setText(myMap.get(ClavesPyJ.horario.toString()).toString());
+        final Button button = findViewById(R.id.btnGM);
 
         if (myMap.get(ClavesPyJ.localizacion.toString()) != null) {
-            loc.setText(myMap.get(ClavesPyJ.localizacion.toString()).toString());
+            //loc.setText(myMap.get(ClavesPyJ.localizacion.toString()).toString());
+            String coordenadas = myMap.get(ClavesPyJ.localizacion.toString()).toString();
+            final String latitud = coordenadas.substring(0, coordenadas.indexOf(' '));
+            final String longitud = coordenadas.substring(coordenadas.indexOf(' ') + 1, coordenadas.length());
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Uri gmmIntentUri = Uri.parse("geo:" + latitud + "," + longitud + "");
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(mapIntent);
+                    }
+                }
+            });
+
         } else {
-            loc.setText(getString(R.string.dato_no_existe));
+            //loc.setText(getString(R.string.dato_no_existe));
+            button.setEnabled(false);
         }
 
         name.setText(myMap.get(ClavesPyJ.nombre.toString()).toString());
